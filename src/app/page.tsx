430503a49d47942b3725037d3e1c4ba1b0606655
NAGARRO\akshaysrivastava <akshaysrivastava46@gmail.com>
2024-05-14 12:50:38 +0530

    actually linking database to the UI

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,33 +3,27 @@ import { db } from "~/server/db";
 
 export const dynamic = "force-dynamic";
 
-const mockUrls = [
-  "https://utfs.io/f/f5818ca7-ae7c-45d4-8e1c-4224613466a1-ibcluw.png",
-  "https://utfs.io/f/ca96259a-b479-4125-9f81-46eb955baabb-1xaic6.jpeg",
-  "https://utfs.io/f/9e2c498e-f3e5-4b5e-817e-714bf8f32138-nm33wo.png",
-  "https://utfs.io/f/07722ba2-b16e-4e06-90f2-a4897bce2437-ibclvr.png",
-  "https://utfs.io/f/2d1b6114-99d9-4bf0-830d-542bb27d5411-kxp3xd.jpg",
-];
-
-const mockImages = mockUrls.map((url, index) => ({
-  id: index + 1,
-  url,
-}));
-
 export default async function HomePage() {
-  const posts = await db.query.posts.findMany();
-  console.log(posts);
+  const images = await db.query.images.findMany({
+    orderBy: (model, { desc }) => desc(model.id),
+  });
+
   return (
     <main>
-      <div>
-        {posts.map((post) => (
-          <div key={post.id}>{post.name}</div>
-        ))}
-      </div>
       <div className="flex flex-wrap gap-4">
-        {[...mockImages, ...mockImages, ...mockImages].map((image, index) => (
-          <div key={image.id + index.toString()} className="w-48">
-            <img src={image.url} alt="image" />
+        {[...images, ...images, ...images].map((image, index) => (
+          <div
+            key={image.id + index.toString()}
+            className="flex h-60 w-48 flex-col"
+          >
+            <div className="h-52 w-48 overflow-hidden">
+              <img
+                src={image.url}
+                className="h-full w-full object-cover"
+                alt="image"
+              />
+            </div>
+            <div>{image.name}</div>
           </div>
         ))}
       </div>
