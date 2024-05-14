5c7b268a370dd89022ec3902525e5f1229f0d13a
NAGARRO\akshaysrivastava <akshaysrivastava46@gmail.com>
2024-05-14 16:01:35 +0530

    ui changes for auth

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,32 +1,45 @@
+import { SignedIn, SignedOut } from "@clerk/nextjs";
 import Link from "next/link";
 import { db } from "~/server/db";
 
 export const dynamic = "force-dynamic";
 
-export default async function HomePage() {
+async function Images() {
   const images = await db.query.images.findMany({
     orderBy: (model, { desc }) => desc(model.id),
   });
+  return (
+    <div className="flex flex-wrap gap-4">
+      {[...images, ...images, ...images].map((image, index) => (
+        <div
+          key={image.id + index.toString()}
+          className="flex h-60 w-48 flex-col"
+        >
+          <div className="h-52 w-48 overflow-hidden">
+            <img
+              src={image.url}
+              className="h-full w-full object-cover"
+              alt="image"
+            />
+          </div>
+          <div>{image.name}</div>
+        </div>
+      ))}
+    </div>
+  );
+}
 
+export default async function HomePage() {
   return (
     <main>
-      <div className="flex flex-wrap gap-4">
-        {[...images, ...images, ...images].map((image, index) => (
-          <div
-            key={image.id + index.toString()}
-            className="flex h-60 w-48 flex-col"
-          >
-            <div className="h-52 w-48 overflow-hidden">
-              <img
-                src={image.url}
-                className="h-full w-full object-cover"
-                alt="image"
-              />
-            </div>
-            <div>{image.name}</div>
-          </div>
-        ))}
-      </div>
+      <SignedOut>
+        <div className="w-full p-4 text-center text-2xl">
+          Please sign in above
+        </div>
+      </SignedOut>
+      <SignedIn>
+        <Images />
+      </SignedIn>
     </main>
   );
 }
